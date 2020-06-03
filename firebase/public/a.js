c3203299695ffb6e1c8b59035737e7547b1761c8
Bobby <bobbylaudeman@gmail.com>
2020-06-02 21:50:01 -0700

    cleaned up some of the code

--- firebase/public/a.js
+++ firebase/public/a.js
@@ -2,7 +2,7 @@
 
 function sendEmailLoginLink()
 {
-  a_send_link_btn.disable = true;
+  a_send_link_btn.disabled = true;
   const email = a_email.value;
   var actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
@@ -23,20 +23,6 @@ function sendEmailLoginLink()
   });
 }
 
-function logout()
-{
-  firebase.auth().signOut()
-  .then(function() {
-    a_msg.innerHTML = "logged out";
-    a_send_link_btn.disable = false;
-    a_logging_in.style.display = "block";
-    a_logged_in.style.display = "none";
-  })
-  .catch(function(error) {
-    console.log(error);
-  })
-}
-
 function loginPageLoad()
 {
   let email = localStorage.getItem("email");
@@ -51,51 +37,40 @@ function loginPageLoad()
   .catch(function(error) {
     console.log(error);
   })
-
-  a_msg.innerHTML = "logged in";
-  a_logging_in.style.display = "none";
-  a_logged_in.style.display = "block";
-  a_logout_btn.addEventListener('click', logout);
 }
 
-function loggedOutPageLoad() {
-  a_msg.innerHTML = "logged out";
-  a_logging_in.style.display = "block";
-  a_send_link_btn.addEventListener('click', sendEmailLoginLink);
-}
-
-function loggedInPageLoad()
+function logout()
 {
-  a_msg.innerHTML = "logged in";
-  a_logged_in.style.display = "block";
-  a_logout_btn.addEventListener('click', logout);
+  a_logout_btn.disabled = true;
+  firebase.auth().signOut().catch(function(error) {
+    console.log(error);
+  })
 }
 
-
-
-
+function authStateChanged(user) {
+  if (user === null) {
+    a_msg.innerHTML = "logged out";
+    a_logging_in.style.display = "block";
+    a_logged_in.style.display = "none";
+    a_send_link_btn.disabled = false;
+  }
+  else {
+    a_msg.innerHTML = "logged in";
+    a_logging_in.style.display = "none";
+    a_logged_in.style.display = "block";
+    a_logout_btn.disable = false;
+  }
+}
 
 document.addEventListener('DOMContentLoaded', f);
 function f() {
   a_msg.innerHTML = "loaded";
+  a_send_link_btn.addEventListener('click', sendEmailLoginLink);
+  a_logout_btn.addEventListener('click', logout);
   const auth = firebase.auth();
+  auth.onAuthStateChanged(authStateChanged);
   if (auth.isSignInWithEmailLink(location.href)) {
     loginPageLoad();
   }
-  else if (auth.currentUser === null) {
-    loggedOutPageLoad();
-  }
-  else
-  {
-    loggedInPageLoad();
-  }
 
-  try {
-    let app = firebase.app();
-    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
-    document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
-  } catch (e) {
-    console.error(e);
-    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-  }
 };
