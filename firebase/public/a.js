c6937979a80d548e691c8bd4dae56e22ee4f6fb8
Bobby <bobbylaudeman@gmail.com>
2020-06-08 23:17:07 -0700

    commit before implementing database

--- firebase/public/a.js
+++ firebase/public/a.js
@@ -1,76 +1,79 @@
-
-
-function sendEmailLoginLink()
-{
-  a_send_link_btn.disabled = true;
-  const email = a_email.value;
-  var actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be whitelisted in the Firebase Console.
-    url: location.href,
-    handleCodeInApp: true
-  };
-
-  firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-  .then(function() {
-    a_msg.innerText = "Login link sent to email."
-    localStorage.setItem("email", email);
-  })
-  .catch(function(error)
+(function () {
+  
+  window.a = {}
+  
+  function sendEmailLoginLink()
   {
-    console.log(error);
+    a_send_link_btn.disabled = true;
+    const email = a_email.value;
+    var actionCodeSettings = {
+      // URL you want to redirect back to. The domain (www.example.com) for this
+      // URL must be whitelisted in the Firebase Console.
+      url: location.href,
+      handleCodeInApp: true
+    };
 
-  });
-}
+    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    .then(function() {
+      a_msg.innerText = "Login link sent to email."
+      localStorage.setItem("email", email);
+    })
+    .catch(function(error)
+    {
+      console.log(error);
 
-function loginPageLoad()
-{
-  let email = localStorage.getItem("email");
-  if (!email) {
-    email = window.prompt("Please provide your email for confirmation");
+    });
   }
 
-  firebase.auth().signInWithEmailLink(email, location.href)
-  .then(function(result) {
-    localStorage.removeItem("email");
+  function loginPageLoad()
+  {
+    let email = localStorage.getItem("email");
+    if (!email) {
+      email = window.prompt("Please provide your email for confirmation");
+    }
+
+    firebase.auth().signInWithEmailLink(email, location.href)
+    .then(function(result) {
+      localStorage.removeItem("email");
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+  }
+
+  function logout()
+  {
+    a_logout_btn.disabled = true;
+    firebase.auth().signOut().catch(function(error) {
+      console.log(error);
+    })
+  }
+
+  function authStateChanged(user) {
+    if (user === null) {
+      a_msg.innerHTML = "logged out";
+      a_logging_in.style.display = "block";
+      a_logged_in.style.display = "none";
+      a_send_link_btn.disabled = false;
+      if (a.logout) a.logout();
+    }
+    else {
+      a_msg.innerHTML = "logged in";
+      a_logging_in.style.display = "none";
+      a_logged_in.style.display = "block";
+      a_logout_btn.disabled = false;
+      if (a.login) a.login();
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    a_msg.innerHTML = "loaded";
+    a_send_link_btn.addEventListener('click', sendEmailLoginLink);
+    a_logout_btn.addEventListener('click', logout);
+    const auth = firebase.auth();
+    auth.onAuthStateChanged(authStateChanged);
+    if (auth.isSignInWithEmailLink(location.href)) {
+      loginPageLoad();
+    }
   })
-  .catch(function(error) {
-    console.log(error);
-  })
-}
-
-function logout()
-{
-  a_logout_btn.disabled = true;
-  firebase.auth().signOut().catch(function(error) {
-    console.log(error);
-  })
-}
-
-function authStateChanged(user) {
-  if (user === null) {
-    a_msg.innerHTML = "logged out";
-    a_logging_in.style.display = "block";
-    a_logged_in.style.display = "none";
-    a_send_link_btn.disabled = false;
-  }
-  else {
-    a_msg.innerHTML = "logged in";
-    a_logging_in.style.display = "none";
-    a_logged_in.style.display = "block";
-    a_logout_btn.disable = false;
-  }
-}
-
-document.addEventListener('DOMContentLoaded', f);
-function f() {
-  a_msg.innerHTML = "loaded";
-  a_send_link_btn.addEventListener('click', sendEmailLoginLink);
-  a_logout_btn.addEventListener('click', logout);
-  const auth = firebase.auth();
-  auth.onAuthStateChanged(authStateChanged);
-  if (auth.isSignInWithEmailLink(location.href)) {
-    loginPageLoad();
-  }
-
-};
+})();
