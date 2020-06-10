77ae2c449a28cead25e0d4b7e813453746df25ff
Bobby <bobbylaudeman@gmail.com>
2020-06-09 18:40:45 -0700

    added test cases for firestore

--- firebase/test/test.js
+++ firebase/test/test.js
@@ -10,5 +10,19 @@ describe("firebase rules test", () => {
     it("non-authenticated users can access nothing", async () => {
         const app = firebase.initializeTestApp({ projectId: projectId, auth: null });
         const db = app.firestore();
+        const aliceRef = db.collection("users").doc("alice");
+        await firebase.assertFails(aliceRef.get());
+    })
+    it("authenticated users can access nothing", async () => {
+        const app = firebase.initializeTestApp({ projectId: projectId, auth: {uid: "alice"} });
+        const db = app.firestore();
+        const aliceRef = db.collection("users").doc("alice");
+        await firebase.assertSucceeds(aliceRef.get());
+    })
+    it("authenticated users can not access other user docs", async () => {
+        const app = firebase.initializeTestApp({ projectId: projectId, auth: {uid: "alice"} });
+        const db = app.firestore();
+        const bobRef = db.collection("users").doc("bob");
+        await firebase.assertFails(bobRef.get());
     })
 })
